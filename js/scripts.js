f3b42add85308b8cee15a734bba915afaf033630
Eric Crudup <briefcasemanx@gmail.com>
2022-01-28 09:06:28 -0800

    creates const list of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+function Cart() {
+
+}
+
+function Pizza()  {
+
+}
+
+function Topping(ToppingListElem)  {
+
+}
+
+
+const toppingList = [ ["pepperoni", 3], 
+                          ["sausage", 3], 
+                          ["mushrooms", 2], 
+                          ["peppers", 2], 
+                          ["car parts", 30] ]
+
+
+
+
+
 $(document).ready(function() {
 
 
